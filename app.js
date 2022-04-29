352d5d7d43dee24a7d901d20408a4895d307c6e6
Theodor Will <theodor.will@gmail.com>
2022-04-29 14:12:02 +0200

    Intitial commit with dependencies

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const express = require('express');
 
+// express app
+const app = express();
+
+/* // register view engine
+app.set('view engine', 'ejs'); */
+
+// Listen for requests
+app.listen(3000);
+
+/* app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+app.use((req, res) => {
+    res.status(404).render('404');
+}) */
