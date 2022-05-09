fa168eb9e7f95f7b398a54f72809bbf7a0863147
Tejaswi Vutukuri <tejaswi.vutukuri@iths.com>
2022-05-09 12:04:42 +0200

    change the endpoint post to postmessage

--- routes/messages.js
+++ routes/messages.js
@@ -5,7 +5,7 @@ const {Router} = require('express')
 const router = new Router()
 
 router.get('/:id/messages', Auth.user, MessageController.getMessages)
-router.post('/:id/post', Auth.user, MessageController.postMessage)
+router.post('/:id/postmessage', Auth.user, MessageController.postMessage)
 // router.patch('/:id/update/:msgid', Auth.user, MessageController.updateMessage)
 // router.delete('/:id/delete/:msgid', Auth.user, MessageController.deleteMessage)
 // router.post('/:id/images', Auth.user, MessageController.uploadImage)
