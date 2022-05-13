e98cbedb35bf6ff922e7354d586512c321093b6d
Tejaswi Vutukuri <tejaswi.vutukuri@iths.com>
2022-05-13 18:12:57 +0200

    Enable web sockets

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const fileUpload = require('express-fileupload')
 
 // express app
 const app = express()
+
+const http = require('http')
+const {Server} = require('socket.io')
+
+const server = http.createServer(app)
+const serverIo = new Server(server)
+
 //middleware
 app.use(Logger)
 app.use(express.json())
 app.use(fileUpload({useTempFiles : true}))
+app.use(express.static('public'))
 
 //routes
 app.use('/api', routes.auth);
@@ -20,10 +28,28 @@ app.use('/api/tasks', routes.tasks);
 app.use('/api/tasks', routes.images);
 app.use(errorHandler)
 
+serverIo
+.on('connection', socket => {
+  console.log("Client connected with ID: " + socket.id);
+
+  socket.on('newMessage', function(msg) {
+    console.log('new message: ', msg)
+  });
+
+  socket.on('disconnect', () => {
+    console.log("Client disconnected");
+  })
+})
+
+var io = require('socket.io-client');
+//var socket = io()
+var socket = io.connect('http://localhost:7000')
+//socket.emit('foo', "Hello Test");
+app.set('clientSocket', socket);
 
 // Listen for requests
 const PORT = process.env.PORT || 7000
-app.listen(PORT, () => console.log(`Server Running on ${PORT}`))
+server.listen(PORT, () => console.log(`Server Running on ${PORT}`))
 
 app.use((req,res)=>{
     res.status(404).json({error:"The requested page is not found"})
